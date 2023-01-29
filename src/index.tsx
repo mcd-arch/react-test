08c18adeecef9319716479c25b4af5452922bcc9
Viktor <cejie3ihka@gmail.com>
2023-01-29 15:58:56 +0200

    react elements creation

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+const h1 = React.createElement(`h1`, { id: `title` }, `hello world`)
+console.log(h1)
+
+const list = React.createElement(
+    `ul`,
+    null,
+    React.createElement(`li`, null, `list item 1`),
+    React.createElement(`li`, null, `list item 2`),
+    React.createElement(`li`, null, `list item 3`)
+)
+
+const content = (
+    <div>
+        {h1}
+        {list}
+    </div>
+)
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode></React.StrictMode>)
+root.render(<React.StrictMode>{content}</React.StrictMode>)

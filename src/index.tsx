519738601c4af21bc9fb0b5237487c0a81b9fe75
Viktor <cejie3ihka@gmail.com>
2023-01-29 17:03:22 +0200

    react fragment

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-const h1 = <h1 id="title">Hello React.js</h1>
+/*const h1 = <h1 id="title">Hello React.js</h1>
 
 let a = 10
 let b = `jfgjdlgdjlfgl`
@@ -19,7 +19,43 @@ const content = (
         {h1}
         {list}
     </div>
-)
+)*/
+
+const Title = () => {
+    return <h1>Hello App</h1>
+}
+
+const Content = () => {
+    return (
+        <React.Fragment>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi
+                libero sapiente nesciunt nemo debitis consequatur culpa nisi
+                dolorum enim cumque, ipsa perspiciatis quam natus tempora earum
+                voluptatum? Veniam, molestiae voluptatum.
+            </p>
+            <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam
+                delectus deleniti quo, eos in rem accusantium, nemo assumenda
+                consequuntur voluptatum distinctio repellat, ipsam sapiente
+                blanditiis consectetur dicta quas ea cum!
+            </p>
+        </React.Fragment>
+    )
+}
+
+function App() {
+    return (
+        <>
+            <Title></Title>
+            <Content></Content>
+        </>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode>{content}</React.StrictMode>)
+root.render(
+    <React.StrictMode>
+        <App></App>
+    </React.StrictMode>
+)

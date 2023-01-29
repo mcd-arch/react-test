1f42bae85514596f2a53fead0b7630680eca79dd
Viktor <cejie3ihka@gmail.com>
2023-01-29 16:18:36 +0200

    added tegs

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-const h1 = React.createElement(`h1`, { id: `title` }, `hello world`)
-console.log(h1)
+const h1 = <h1 id="title">Hello React.js</h1>
 
-const list = React.createElement(
-    `ul`,
-    null,
-    React.createElement(`li`, null, `list item 1`),
-    React.createElement(`li`, null, `list item 2`),
-    React.createElement(`li`, null, `list item 3`)
+const list = (
+    <ul>
+        <li>list item 1</li>
+        <li>list item 2</li>
+        <li>list item 3</li>
+    </ul>
 )
 
 const content = (

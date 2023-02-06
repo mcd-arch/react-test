67e6612aba415e26d6f7dabe4f34c6ed5a13f6b3
Viktor <cejie3ihka@gmail.com>
2023-02-06 23:19:47 +0200

    logo added

---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,14 @@
+import logo from 'assets/logo.svg'
+import Typography from '@mui/material/Typography'
+
+type Props = {}
+
+const Logo = (props: Props) => {
+    return (
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            <img src={logo} alt="logopic"></img>
+        </Typography>
+    )
+}
+
+export default Logo

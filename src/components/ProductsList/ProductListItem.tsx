5bd00515f32cac50f9f687e383c221f93e4a008c
Viktor <cejie3ihka@gmail.com>
2023-02-07 17:09:45 +0200

    props to carts added

--- src/components/ProductsList/ProductListItem.tsx
+++ src/components/ProductsList/ProductListItem.tsx
@@ -1,17 +1,25 @@
 import { Card, CardContent, Button, CardActions } from '@mui/material'
 import './ProductListItem.scss'
 
-type Props = {}
+type Props = {
+    title: string
+    description: string
+    type: string
+    capasity: number
+    price: number
+}
 
 const ProductListItem = (props: Props) => {
     return (
         <Card variant="outlined" className="product">
             <CardContent>
-                <div className="product_title">iPhone 14 PRO</div>
-                <div className="product_description">This iPhone 14 PRO</div>
-                <div className="product_features">Type: phone</div>
-                <div className="product_features">Capasity: 256 Gb</div>
-                <div className="product_price">1200$</div>
+                <div className="product_title">{props.title}</div>
+                <div className="product_description">{props.description}</div>
+                <div className="product_features">Type: {props.type}</div>
+                <div className="product_features">
+                    Capasity: {props.capasity} Gb
+                </div>
+                <div className="product_price">{props.price}$</div>
             </CardContent>
             <CardActions className="product_btn_wrap">
                 <Button variant="outlined">Add to cart</Button>
--- src/components/ProductsList/ProductsList.tsx
+++ src/components/ProductsList/ProductsList.tsx
@@ -12,22 +12,58 @@ const ProductsList = (props: Props) => {
 
             <Grid container spacing={3}>
                 <Grid item xs={12} sm={6} md={4}>
-                    <ProductListItem />
+                    <ProductListItem
+                        title="iPhone 13 Pro"
+                        description="iPhone"
+                        type="phone"
+                        capasity={256}
+                        price={1299}
+                    />
                 </Grid>
                 <Grid item xs={12} sm={6} md={4}>
-                    <ProductListItem />
+                    <ProductListItem
+                        title="iPhone 12 Pro"
+                        description="iPhone"
+                        type="phone"
+                        capasity={128}
+                        price={1099}
+                    />
                 </Grid>
                 <Grid item xs={12} sm={6} md={4}>
-                    <ProductListItem />
+                    <ProductListItem
+                        title="iPhone 13 Pro"
+                        description="iPhone"
+                        type="phone"
+                        capasity={64}
+                        price={999}
+                    />
                 </Grid>
                 <Grid item xs={12} sm={6} md={4}>
-                    <ProductListItem />
+                    <ProductListItem
+                        title="iPhone 10 Pro"
+                        description="iPhone"
+                        type="phone"
+                        capasity={64}
+                        price={999}
+                    />
                 </Grid>
                 <Grid item xs={12} sm={6} md={4}>
-                    <ProductListItem />
+                    <ProductListItem
+                        title="iPhone X Pro"
+                        description="iPhone"
+                        type="phone"
+                        capasity={128}
+                        price={899}
+                    />
                 </Grid>
                 <Grid item xs={12} sm={6} md={4}>
-                    <ProductListItem />
+                    <ProductListItem
+                        title="iPhone 8"
+                        description="iPhone"
+                        type="phone"
+                        capasity={32}
+                        price={399}
+                    />
                 </Grid>
             </Grid>
         </>
